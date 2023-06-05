02a3201a36240c16a989cac37e8761e70076cbbc
Ramdhas <ramdhas.m@nexergroup.com>
2023-06-05 10:47:07 +0200

    Initial Commit

---
+++ RepoSurfer/RepoSurferApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoSurferApp.swift
+//  RepoSurfer
+//
+//  Created by Ramdhas on 2023-06-05.
+//
+
+import SwiftUI
+
+@main
+struct RepoSurferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
